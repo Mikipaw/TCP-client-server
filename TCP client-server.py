979b0ec79be68a55cbcd2ef7553816ae0dc7207b
Mikipaw <mapawlow@yandex.ru>
2022-07-05 19:15:17 +0300

    First commit

---
+++ TCP client-server.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from socket import *
+import sys
+
+#serverName = 'hostname'
+#serverPort = 12000
+
+BUFFER_SIZE      = 2048
+PORT_LOWER_BOUND = 1024
+PORT_UPPER_BOUND = 5000
